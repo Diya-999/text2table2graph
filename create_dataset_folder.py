b302fc07e2f825c0a7f944554aa13d8b406b53de
diya <diya.dai@fcc-analytics.com>
2024-03-06 18:21:27 +0800

    feat: filter node size

--- create_dataset_folder.py
+++ create_dataset_folder.py
@@ -57,6 +57,20 @@ def cut_invalid_header(df_row):
             df_row['df_graph_ind'] = 'after_cut_header'
     return df_row  
 
+def find_max_node_size(df_row):
+    word_re = re.compile(r'[0-9A-Za-z\'\’]+')
+    df_row['df_graph'][['from_node_size','to_node_size']] = 0
+    for index, row in df_row['df_graph'].iterrows():
+        if row['c1']:
+            c1_num = len(re.findall(word_re,row['c1']))
+            df_row['df_graph'].loc[index,'from_node_size'] = c1_num
+        if row['c2']:
+            c2_num = len(re.findall(word_re,row['c2']))
+            df_row['df_graph'].loc[index,'to_node_size'] = c2_num
+    df_row['max_node_size'] = df_row['df_graph']['from_node_size'].max() if  df_row['df_graph']['from_node_size'].max() > df_row['df_graph']['to_node_size'].max() else df_row['df_graph']['to_node_size'].max()
+    df_row['min_node_size'] = df_row['df_graph']['from_node_size'].min() if df_row['df_graph']['from_node_size'].min() < df_row['df_graph']['to_node_size'].min() else df_row['df_graph']['to_node_size'].min()
+    return df_row
+
 def build_txt_table(df_row):
     if df_row['df_graph_ind'] == "after_cut_header":
         result_str = '|'
@@ -75,6 +89,8 @@ parser.add_argument('--valid_ratio', type=float, default=0.1)
 parser.add_argument('--train_ratio', type=float, default=0.9)
 parser.add_argument('--input_path', type=str, default='./pkl/final_rs_on_news_v2.pkl')
 parser.add_argument('--output_path', type=str, default='./dataset/')
+parser.add_argument('--filter_by_max_node_size', type=int, default=20)
+parser.add_argument('--filter_by_min_node_size', type=int, default=0)
 
 args = parser.parse_args()
 
@@ -91,15 +107,17 @@ df_4_sep = df_4_sep.assign(df_graph_ind=df_4_sep['df_graph'].apply(check_header)
 df_4_sep = df_4_sep.apply(cut_normal_header, axis=1)
 df_4_sep = df_4_sep.apply(check_and_rename_empty_node, axis=1)
 df_4_sep = df_4_sep.apply(cut_invalid_header, axis=1)
+df_4_sep = df_4_sep.apply(find_max_node_size, axis=1)
+
 df_4_sep = df_4_sep.apply(build_txt_table, axis=1)
 
 total_num = df_4_sep.query("df_graph_ind=='after_cut_header'").shape[0]
 total_train_num = int(total_num*args.train_ratio)
 val_num = int(total_train_num*args.valid_ratio)
-val_index = df_4_sep.query("df_graph_ind=='after_cut_header'").index[:val_num]
-train_index = df_4_sep.query("df_graph_ind=='after_cut_header'").index[val_num:total_train_num]
-test_index = df_4_sep.query("df_graph_ind=='after_cut_header'").index[total_train_num:]
-
+filter_index = df_4_sep.query("df_graph_ind=='after_cut_header'").query(f"max_node_size <= {args.filter_by_max_node_size}").query(f"min_node_size >= {args.filter_by_min_node_size}").index
+val_index = filter_index[:val_num]
+train_index = filter_index[val_num:total_train_num]
+test_index = filter_index[total_train_num:]
 
 if not os.path.exists(args.output_path):
     os.makedirs(args.output_path)
