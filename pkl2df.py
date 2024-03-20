025c423dbb5de59d45686a35f0a57501225df4e2
diya <diya.dai@fcc-analytics.com>
2024-03-20 18:00:50 +0800

    refactor: load new format pkl dataset

--- pkl2df.py
+++ pkl2df.py
@@ -6,8 +6,9 @@ def pkl2df(file_folder):
     df_ls = []
     for index,filename in enumerate(os.listdir(file_folder)):
         df_tmp = pd.read_pickle(os.path.join(file_folder, filename))
-        if set(df_tmp.columns) == set({'entity 1', 'entity 2', 'relation', 'source'}):
-            df_ls.append(df_tmp)
+        assert 'json' in df_tmp.columns
+        df = pd.DataFrame(df_tmp['json'][0])
+        df_ls.append(df)
     df = pd.concat(df_ls).reset_index(drop=True)
     df = df[~df['entity 1'].isnull()]
     df = df[~df['entity 2'].isnull()]
