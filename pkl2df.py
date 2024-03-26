0262f5e5754b8d745d2bb89944b8b5baa2e5633f
diya <diya.dai@fcc-analytics.com>
2024-03-26 19:09:28 +0800

    add flexible controls in pkl2df.py

--- pkl2df.py
+++ pkl2df.py
@@ -1,43 +1,39 @@
 import pandas as pd
-import os
+import os, json
 import argparse
 
-def pkl2df(file_folder):
+def pkl2df(is_value_str, file_folder, extract_column_list):
     df_ls = []
     for index,filename in enumerate(os.listdir(file_folder)):
         df_tmp = pd.read_pickle(os.path.join(file_folder, filename))
-        assert 'json' in df_tmp.columns
-        df = pd.DataFrame(df_tmp['json'][0])
+        if is_value_str:
+            df_columns = [{k:str(v) for k,v in sample.items() if k in extract_column_list} for sample in df_tmp['json'][0]]
+        else:
+            df_columns = [{k:v for k,v in sample.items() if k in extract_column_list} for sample in df_tmp['json'][0]]
+
+        df = pd.DataFrame(df_columns)
         df_ls.append(df)
     df = pd.concat(df_ls).reset_index(drop=True)
-    df = df[~df['entity 1'].isnull()]
-    df = df[~df['entity 2'].isnull()]
     df = df.reset_index(drop=True)
-    df['attr'] =  [{'entity 1':{'shape':'dot'},'entity 2':{'shape':'dot'}}]*df.shape[0]
-    return df
-
-def get_news_sum(file_folder):
-    sum_dict = {}
-    for filename in os.listdir(file_folder):
-        df_tmp = pd.read_pickle(os.path.join(file_folder, filename))
-        assert 'json' in df_tmp.columns and len(df_tmp['json'][0]) == 1
-        tmp_name = filename[:filename.rindex(".")]
-        sum_dict[tmp_name] = df_tmp['json'][0][0]['text']
-    df = pd.DataFrame({'source':sum_dict.keys(),'summary':sum_dict.values()})
     return df
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_folder', type=str, default='./dj_news/')
+    parser.add_argument('--extract_column_list', type=str, nargs='+', default=[])
     parser.add_argument('--output_path', type=str, default='./df.pkl')
-    parser.add_argument('--input_sum_folder', type=str, default='')
-    parser.add_argument('--output_sum_path', type=str, default='./df_sum.pkl')
-
+    parser.add_argument('--map_cols', type=str, default='')
+    parser.add_argument('--query_str', type=str, default='')
+    parser.add_argument('--is_value_str', type=int, default=1)
+    
     args = parser.parse_args()
 
-    df = pkl2df(args.input_folder)
-    df.to_pickle(args.output_path)
+    df = pkl2df(args.is_value_str, args.input_folder, args.extract_column_list)
+    if args.map_cols:
+        columns = json.loads(args.map_cols)
+        df = df.rename(columns=columns)
 
-    if args.input_sum_folder:
-        df_sum = get_news_sum(args.input_sum_folder)
-        df_sum.to_pickle(args.output_sum_path)
+    if args.query_str:
+        df = df.query(args.query_str).reset_index(drop=True)
+
+    df.to_pickle(args.output_path)
